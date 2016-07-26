401a8ea5dc4e4e963b13b2317fa4946cbc538c04
Isaac Whitfield <iwhitfield@appcelerator.com>
2016-07-25 22:29:06 -0400

    Bump to v1.0.1

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Sentix.Mixfile do
         },
         maintainers: [ "Isaac Whitfield" ]
       },
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.2",
       deps: deps,
       docs: [
